849d9a797c62a57f20b042b62a6c4ca0372f4be9
yuriygit <yuriybpost@gmail.com>
2015-06-22 01:02:12 +0300

    added classes Price, Product

---
+++ src/main/java/com/yuriyb/pointofsale/productprices/Price.java
@@ -0,0 +1,73 @@
+package com.yuriyb.pointofsale.productprices;
+
+import java.math.BigDecimal;
+import java.util.Currency;
+import java.util.Locale;
+
+/**
+ * Price class. It describes product price. It includes currency.
+ * @version 1.80 12 April 2015
+ * @author  Yuriy B.
+ */
+public class Price {
+
+	private BigDecimal value;
+	private Currency currency;
+	
+	/**
+	 * Constructor of Price. It creates a new Price with specified <tt>value</tt> and <tt>currency</tt>.
+	 * 
+	 * @param value    price value
+     * @param currency defines the currency of the price
+	 */
+	public Price(BigDecimal value, Currency currency){
+		this.value = value;
+		this.currency = currency;
+	}
+	
+	/**
+     * Creates a new Price with specified <tt>value</tt> in US currency.
+     *
+     * @param value price value
+     */
+	public Price(BigDecimal value){
+		this.value = value;
+		currency = Currency.getInstance(Locale.US);
+	}
+	
+	/**
+     * Gets the value of the price.
+     */
+	public BigDecimal getValue() {
+		return value;
+	}
+
+	/**
+     * Sets the value of the price.
+     */
+	public void setValue(BigDecimal value) {
+		this.value = value;
+	}
+
+	/**
+     * Gets the currency of the price.
+     */
+	public Currency getCurrency() {
+		return currency;
+	}
+
+	/**
+     * Sets the currency of the price.
+     */
+	public void setCurrency(Currency currency) {
+		this.currency = currency;
+	}
+	
+	/**
+     * Overriden toString() method for class Price
+     */
+	@Override
+	public String toString(){
+		return value + currency.getSymbol();
+	}
+}
---
+++ src/main/java/com/yuriyb/pointofsale/productprices/Product.java
@@ -0,0 +1,71 @@
+package com.yuriyb.pointofsale.productprices;
+
+
+/**
+ * Product class. It describes product.
+ * @version 1.80 12 April 2015
+ * @author  Yuriy B.
+ */
+public class Product {
+	
+	private String code;
+	
+	private String title;
+	
+	private Price price;
+	
+	/**
+     * Creates the Product and fills fields.
+     *
+     * @param code  product's code
+     * @param title product title
+     */
+	public Product (String code, String title, Price price) {
+		this.code = code;
+		this.title = title;
+		this.price = price;
+	}
+	
+	/**
+     * Gets the code of the product
+     */
+	public String getCode() {
+		return code;
+	}
+	
+	/**
+     * Sets the code of the product
+     */
+	public void setCode(String code) {
+		this.code = code;
+	}
+	
+	/**
+     * Gets the title of the product
+     */
+	public String getTitle() {
+		return title;
+	}
+	
+	/**
+     * Sets the title of the product
+     */
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	/**
+     * Gets the price of the product
+     */
+	public Price getPrice() {
+		return price;
+	}
+	
+	/**
+     * Sets the price of the product
+     */
+	public void setPrice(Price price){
+		this.price = price;
+	}
+
+}
