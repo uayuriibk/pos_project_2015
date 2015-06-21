97b7a192d0c680ad5b24c9548c977c7c26961784
Yuriy B <yuriygit@users.noreply.github.com>
2015-06-22 01:33:32 +0300

    Update IProductsInfoDB.java

--- src/main/java/com/yuriyb/pointofsale/productprices/IProductsInfoDB.java
+++ src/main/java/com/yuriyb/pointofsale/productprices/IProductsInfoDB.java
@@ -5,23 +5,23 @@ import com.yuriyb.pointofsale.exceptions.UndefinedProductException;
 public interface IProductsInfoDB {
 
 	/**
-     * Adds the product into ProductsWithPricesStorage in field productsWithCodesMap
+     * Adds the product into IProductsInfoDB in field productsWithCodesMap
      */
 	public void addProduct(Product product);
 
 	/**
-     * Deletes the product from ProductsWithPricesStorage by product code
+     * Deletes the product from IProductsInfoDB by product code
      */
 	public void deleteProduct(String productCode);
 
 	/**
-     * Gets the product price from ProductsWithPricesStorage by product code
+     * Gets the product price from IProductsInfoDB by product code
 	 * @throws UndefinedProductException 
      */
 	public Price getPrice(String productCode);
 
 	/**
-     * Checks the product presence in the ProductsWithPricesStorage by product code
+     * Checks the product presence in the IProductsInfoDB by product code
      */
 	public boolean isPresentInStorage(String productCode);
 }
