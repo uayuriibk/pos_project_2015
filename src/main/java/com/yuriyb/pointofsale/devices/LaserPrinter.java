3ab2553a8559f2b4843dc28f6727d068a4c52870
yuriygit <yuriybpost@gmail.com>
2015-06-22 17:16:48 +0300

    fix in LaserPrinter

--- src/main/java/com/yuriyb/pointofsale/devices/LaserPrinter.java
+++ src/main/java/com/yuriyb/pointofsale/devices/LaserPrinter.java
@@ -2,14 +2,6 @@ package com.yuriyb.pointofsale.devices;
 
 public class LaserPrinter implements IPrinter {
 	
-	private static IPrinter instance = new LaserPrinter();
-	
-	private LaserPrinter(){
-	}
-	
-	public static synchronized IPrinter getInstance(){
-		return instance;
-	}
 	public void printMessage(String message){
 		System.out.println("LaserPrinter :"+message);
 	}
