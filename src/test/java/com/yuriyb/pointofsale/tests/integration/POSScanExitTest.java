672c2c775de82e0b995f32e2170ddfe13c831785
yuriygit <yuriybpost@gmail.com>
2015-06-22 15:23:16 +0300

    correction in test

--- src/test/java/com/yuriyb/pointofsale/tests/integration/POSScanExitTest.java
+++ src/test/java/com/yuriyb/pointofsale/tests/integration/POSScanExitTest.java
@@ -1,11 +1,23 @@
 package com.yuriyb.pointofsale.tests.integration;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import junit.framework.Assert;
+
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.*;
+import static org.mockito.Mockito.*;
 
+import com.yuriyb.pointofsale.PointOfSale;
 import com.yuriyb.pointofsale.PointOfSaleBuilder;
 import com.yuriyb.pointofsale.PointOfSaleBuildingDirector;
 import com.yuriyb.pointofsale.StandartPointOfSaleBuilder;
+import com.yuriyb.pointofsale.devices.IDisplay;
+import com.yuriyb.pointofsale.devices.IPrinter;
+import com.yuriyb.pointofsale.devices.LCDDisplay;
+import com.yuriyb.pointofsale.devices.LaserPrinter;
 
 public class POSScanExitTest {
 	
@@ -15,12 +27,39 @@ public class POSScanExitTest {
 		PointOfSaleBuilder standartPointOfSale = new StandartPointOfSaleBuilder();
 		director.setPointOfSaleBuilder(standartPointOfSale);
 		director.constructConstructPointOfSale();
-	    //pos = director.getPointOfSale();
-	    //pos.getScaner().setProductsPrices(DataForTestUtility.getProductsInfoDB());
+	    director.getPointOfSale().getScaner().setProductsPrices(DataForTestUtility.getProductsInfoDB());
 	}
 	
 	@Test
-	public void dsfds(){
-		
+	public void checkSize(){
+		List<String> inputData = new ArrayList<String>();
+		for(String input:inputData){
+			PointOfSale.getInstance().processInput(input);
+		}
 	}
+	
+	@Test
+	public void scanCodes1(){
+		IPrinter printerSpy = Mockito.spy(LaserPrinter.getInstance());
+		IDisplay displaySpy = Mockito.spy(LCDDisplay.getInstance());
+		List<String> inputData = new ArrayList<String>();
+		inputData.add("A");
+		inputData.add("B");
+		inputData.add("exit");
+		for(String input:inputData){
+			PointOfSale.getInstance().processInput(input);
+		}
+		
+		verify(printerSpy).printMessage("");//доробити
+		verify(displaySpy).showMessage("");
+	}
+	
+	@Test
+	public void scanCodes(){
+		List<String> inputData = new ArrayList<String>();
+		for(String input:inputData){
+			PointOfSale.getInstance().processInput(input);
+		}
+	}
+	
 }
