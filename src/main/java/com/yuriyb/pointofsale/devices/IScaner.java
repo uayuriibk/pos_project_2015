e20da36f955dc86d792d978bbe00ffa539876ede
yuriygit <yuriybpost@gmail.com>
2015-06-22 15:16:22 +0300

    added method in interface of Scaner

--- src/main/java/com/yuriyb/pointofsale/devices/IScaner.java
+++ src/main/java/com/yuriyb/pointofsale/devices/IScaner.java
@@ -1,5 +1,7 @@
 package com.yuriyb.pointofsale.devices;
 
+import java.util.Map;
+
 import com.yuriyb.pointofsale.exceptions.InvalidBarCodeException;
 import com.yuriyb.pointofsale.exceptions.UndefinedProductException;
 import com.yuriyb.pointofsale.productprices.IProductsInfoDB;
@@ -8,4 +10,5 @@ public interface IScaner {
 	public void scan(String productCode) throws UndefinedProductException, InvalidBarCodeException;
 	public void setProductsPrices(IProductsInfoDB productsInfoDB);
 	public void clearScanned();
+	public Map<String, String> getReceipt() throws UndefinedProductException;
 }
