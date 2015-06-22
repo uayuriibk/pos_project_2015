0c23b66367577196e613dc4c1b486d4935026e14
yuriygit <yuriybpost@gmail.com>
2015-06-22 22:40:04 +0300

    added 2 tests for LaserPrinter, LCDDisplay

---
+++ src/test/java/com/yuriyb/pointofsale/tests/module/LCDDisplayTest.java
@@ -0,0 +1,30 @@
+package com.yuriyb.pointofsale.tests.module;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+
+import com.yuriyb.pointofsale.devices.IDisplay;
+import com.yuriyb.pointofsale.devices.LCDDisplay;
+
+public class LCDDisplayTest {
+	
+	IDisplay display;
+	IDisplay displaySpy;
+	
+	@Before
+	public void setUp(){
+		display = new LCDDisplay();
+		displaySpy = Mockito.spy(display);
+	}
+	
+	@Test
+	public void checkMessageShowingByDisplay(){
+		displaySpy.showMessage("Test");
+		verify(displaySpy).showMessage("Test");
+	}
+}
---
+++ src/test/java/com/yuriyb/pointofsale/tests/module/LaserPrinterTest.java
@@ -0,0 +1,28 @@
+package com.yuriyb.pointofsale.tests.module;
+
+import static org.mockito.Mockito.verify;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import com.yuriyb.pointofsale.devices.IPrinter;
+import com.yuriyb.pointofsale.devices.LaserPrinter;
+
+public class LaserPrinterTest {
+	
+	IPrinter printer;
+	IPrinter printerSpy;
+	
+	@Before
+	public void setUp(){
+		printer = new LaserPrinter();
+		printerSpy = Mockito.spy(printer);
+	}
+	
+	@Test
+	public void checkMessageShowingByDisplay(){
+		printerSpy.printMessage("Test");
+		verify(printerSpy).printMessage("Test");
+	}
+}
