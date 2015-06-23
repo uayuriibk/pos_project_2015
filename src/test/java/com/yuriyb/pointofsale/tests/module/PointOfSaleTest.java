84c569fbe6a2db4a9834bc947d174b4a93b00678
yuriygit <yuriybpost@gmail.com>
2015-06-23 03:07:15 +0300

    added tests for PointOfSale

--- src/test/java/com/yuriyb/pointofsale/tests/module/PointOfSaleTest.java
+++ src/test/java/com/yuriyb/pointofsale/tests/module/PointOfSaleTest.java
@@ -1,32 +1,40 @@
 package com.yuriyb.pointofsale.tests.module;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
+
+import static org.mockito.Mockito.*;
 
 import com.yuriyb.pointofsale.PointOfSale;
-import com.yuriyb.pointofsale.devices.IDisplay;
-import com.yuriyb.pointofsale.devices.IPrinter;
-import com.yuriyb.pointofsale.devices.IScanner;
+import com.yuriyb.pointofsale.exceptions.ProductNotFoundException;
+import com.yuriyb.pointofsale.handlers.ExitHandler;
 import com.yuriyb.pointofsale.handlers.Handler;
+import com.yuriyb.pointofsale.handlers.ScanBarCodeHandler;
 
 public class PointOfSaleTest {
 	
 	PointOfSale pos;
-	IDisplay displayMock;
-	IPrinter printerMock;
-	IScanner scanerMock;
-	private List<Handler> handlers;
 	
 	@Before
 	public void setUp(){
-		
+		pos = PointOfSale.getInstance();
 	}
 	
 	@Test
-	public void testSOMETHIN(){
-		//when()
+	public void testInputProcessingByFirstHandlerInTheChain() throws ProductNotFoundException{
+		Handler exitHandlerMock = Mockito.mock(ExitHandler.class);
+		Handler scanBarCodeHandlerMock = Mockito.mock(ScanBarCodeHandler.class);
+		List<Handler> handlersCollection = new ArrayList<Handler>();
+		handlersCollection.add(exitHandlerMock);
+		handlersCollection.add(scanBarCodeHandlerMock);
+		pos.setHandlersChain(handlersCollection);
+		pos.processInput("Test Input");
+		verify(exitHandlerMock, times(1)).process(anyString());
+		verify(scanBarCodeHandlerMock, times(0)).process(anyString());
 	}
 	
 }
