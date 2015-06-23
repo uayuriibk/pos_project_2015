db57a44b2274525f403ccf386fdac1c636b61f05
yuriygit <yuriybpost@gmail.com>
2015-06-23 04:21:42 +0300

    fix in Handler

--- src/main/java/com/yuriyb/pointofsale/handlers/Handler.java
+++ src/main/java/com/yuriyb/pointofsale/handlers/Handler.java
@@ -1,7 +1,5 @@
 package com.yuriyb.pointofsale.handlers;
 
-import com.yuriyb.pointofsale.exceptions.ProductNotFoundException;
-
 public interface Handler {
 	public void setNext(Handler nextInChain);
 	public void process(String scanedCode);
